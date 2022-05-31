06e7b71455e38efd7166f79041889e32a45fb86a
devmelonlee <devmelonlee@gmail.com>
2022-05-31 23:55:10 +0900

    [ADD] diary

---
+++ TodayYou/Diary.swift
@@ -0,0 +1,16 @@
+//
+//  Diary.swift
+//  TodayYou
+//
+//  Created by 이승혁 on 2022/05/31.
+//
+
+import Foundation
+
+struct Diary {
+  var uuidString: String
+  var title: String
+  var contents: String
+  var date: Date
+  var isStar: Bool
+}
---
+++ TodayYou/DiaryCell.swift
@@ -0,0 +1,13 @@
+//
+//  DiaryCell.swift
+//  TodayYou
+//
+//  Created by 이승혁 on 2022/05/31.
+//
+
+import UIKit
+
+class DiaryCell: UICollectionViewCell {
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var dateLabel: UILabel!  
+}
