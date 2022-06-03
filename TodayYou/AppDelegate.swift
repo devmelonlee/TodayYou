5274bb4ccb1a423856b9d58a25248e0da159fef8
devmelonlee <devmelonlee@gmail.com>
2022-06-03 23:51:57 +0900

    Use snapkit

--- TodayYou/AppDelegate.swift
+++ TodayYou/AppDelegate.swift
@@ -13,8 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
-    //Thread.sleep(forTimeInterval: 5.0) //LaunchScreen Delay 2sec
     sleep(2)
     return true
   }
